cd8f0f31a2ada8cfbc467b819aa3b735a872fcc9
Elise Aaron <ea554820@ohio.edu>
2021-06-17 14:42:18 -0400

    Generation and analysis broken up. Generation written.

--- randomwalk.cpp
+++ randomwalk.cpp
@@ -11,98 +11,61 @@
 using namespace std;
 //namespace plt = matplotlibcpp;
 
-///TEMPLATES
-void runSpecs(int &steps, int &runs); //Process user input for number of steps & runs
-void history(int steps); //Generate a single history and print to file
-//void compute(int runs, Distribution &p, Distribution &q, Distribution &r, Distribution &s); ///Update quantities
-//Data distributions
-Distribution buildRSquare();
-Distribution buildSDist();
-Distribution buildTDist();
-Distribution buildFs();
-//Theoretical distributions
-Distribution buildRSquareThy(int steps); //Build theoretical mean square displacement wrt time
-Distribution buildSDistThy(vector<double> x); //Build theoretical space prob. dist.
-Distribution buildTDistThy(int steps); //Build theoretical time prob. dist.
-Distribution buildFsThy(int steps); //Build theoretical self-intermediate scattering function wrt time
-//Plotting
-void makePlots(Distribution data, Distribution theory); //Build a single plot of data alongside theoretical function
+///FUNCTION DECLARATIONS (DEFINITIONS BELOW MAIN)
+void runSpecs(int &num_histories, int &num_steps); //Process user input for number of runs and steps per run
+void history(int num_steps, string filename); //Generate a single history and print to file
+void recordHistories(int num_histories, int num_steps); //Generate histories and generate datafiles for specified number of runs
+string getFilename(); //Generate destination filename for data
+
 
 int main() {
-    int steps; ///Number of steps in a history 
-    int runs; ///Number of histories to generate
-    Distribution m_square, m_square_thy; ///Mean square displacement, data and theory
-    Distribution sdist, sdist_thy; ///Spatial distribution, data and theory
-    Distribution tdist, tdist_thy; ///Time distribution, data and theory
-    Distribution f_s, f_s_thy; ///Self-intermediate scattering function, data and theory
-    //..
+    int num_histories;
+    int num_steps;
+    runSpecs(num_histories, num_steps);
+    //recordHistories(num_histories, num_steps);
     return 0;
 } ///main
 
-void runSpecs(int &steps, int &runs) {
-    //..
+void runSpecs(int &num_histories, int &num_steps) {
+    cout << "Number of histories: ";
+    cin >> num_histories;
+    cout << "Number of steps per history: ";
+    cin >> num_steps;
 }
 
-void history(int steps) {
-    ///Open a file stream
-    string filename = "data.txt";
+void history(int num_steps, string filename) {
+    ///Open file stream
     ofstream file;
     file.open(filename);
 
-    ///Prepare for generation
+    ///Seed random number generator
     srand(time(0));
-    double step = 0.00;
 
     ///Generate
-    for(int i = 0; i < steps; i++){
-        step = rand() % 1;
-        if(step >= 0.5) {
-            step = 1;
-        }
-        else step = -1;
+    int step = 0;
+    for(int i = 0; i < num_steps; i++){
+        step = rand() % 1 - 1;
         file << step << endl;
     }
-
     ///Close file stream
     file.close();
 }
 
-Distribution buildRSquare() {
-    //..
-    //return..
+void recordHistories(int num_histories, int num_steps) {
+    for(int i; i <= num_histories; i++) {
+        history(num_steps, getFilename());
+    }
 }
 
-Distribution buildSDist() {
-    //..
-    //return..
-}
-
-Distribution buildTDist() {
-    //..
-    //return..
-}
-
-Distribution buildFs() {
-    //..
-    //return..
-}
-
-Distribution buildRSquareThy(int steps) { 
-    //..
-    //return...
-}
-
-Distribution buildSDistThy(vector<double> x) {
-    //..
-    //return..
-}
-
-Distribution buildTDistThy(int steps) {
-    //..
-    //return..
-}
-
-Distribution buildFsThy(int steps) {
-    //..
-    //return..
+string getFilename() {
+    ifstream name;
+    char number = '0';
+    string fullname = "";
+    do { //Search for filename that does not yet exist, and create the file
+        fullname = "history" + to_string(number) + ".txt";
+        name.open(fullname);
+        number++;
+    } while(!name.eof()); //if file is not blank (i.e. already exists), try again
+    name.close();
+    return fullname;
 }
---
+++ randomwalk_analysis.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+    //..
+    return 0;
+}
