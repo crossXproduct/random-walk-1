c5a30452efb46f7318cad8a91887bda44dc8d069
Elise Aaron <ea554820@ohio.edu>
2021-06-27 00:58:14 -0400

    Plot of probability data vs hand check

--- randomwalk_plots.py
+++ randomwalk_plots.py
@@ -1,15 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-X1, Y1 = np.loadtxt('mean_squares_thy.dat', delimiter=',', unpack=True)
-X2, Y2 = np.loadtxt('mean_squares.dat', delimiter=',', unpack=True)
+#mean square displacement, data vs theory
+#X1, Y1 = np.loadtxt('mean_squares_thy.dat', delimiter=',', unpack=True)
+#X2, Y2 = np.loadtxt('mean_squares.dat', delimiter=',', unpack=True)
+
+#probability distribution, data vs hand check
+Y1 = (0,0,0,0,0.333,0,0.333,0,0.333,0)
+X1 = (0,1,2,3,4,5,6,7,8,9)
+
+X2, Y2 = np.loadtxt('p_dist_t3.dat', delimiter=',', unpack=True)
+X2 = (0,1,2,3,4,5,6,7,8,9)
+
 
 plt.scatter(X1, Y1, color='cyan')
 #plt.Line2D(X1, Y1, color='red')
 plt.scatter(X2, Y2, color='orange')
 
-plt.title('Mean Square Displacement as a Function of Time')
-plt.xlabel('time (s)')
-plt.ylabel('mean square displacement (arb. units)')
-plt.legend(["Theory","Data"])
+plt.title('Probability Distribution at t=3s')
+plt.xlabel('displacement (arb. units)')
+plt.ylabel('Probability')
+plt.legend(["Data","Hand Check"])
 plt.show()
