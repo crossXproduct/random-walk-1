cf9bf00e7102c3407f31695606a542d03ca57ed7
Elise Aaron <ea554820@ohio.edu>
2021-07-06 11:20:58 -0400

    Plots of data dists alongside hand checks

--- randomwalk_plots.py
+++ randomwalk_plots.py
@@ -5,20 +5,39 @@ import numpy as np
 #X1, Y1 = np.loadtxt('mean_squares_thy.dat', delimiter=',', unpack=True)
 #X2, Y2 = np.loadtxt('mean_squares.dat', delimiter=',', unpack=True)
 
+#mean square displacement, data vs hand check
+XMSH = (0,1,2,3,4)
+YMSH = np.loadtxt('handcheck_mean_squares.dat', unpack=True)
+XMS, YMS = np.loadtxt('mean_squares.dat', delimiter=',', unpack=True)
+
 #probability distribution, data vs hand check
-Y1 = (0,0,0,0,0.333,0,0.333,0,0.333,0)
-X1 = (0,1,2,3,4,5,6,7,8,9)
+XP1H = (0,1,2,3,4,5,6,7,8)
+YP1H = np.loadtxt('handcheck_p_dist_t1.dat', unpack=True)
+XP1, YP1 = np.loadtxt('p_dist_t1.dat', delimiter=',', unpack=True)
+XP2H = (0,1,2,3,4,5,6,7,8)
+YP2H = np.loadtxt('handcheck_p_dist_t2.dat', unpack=True)
+XP2, YP2 = np.loadtxt('p_dist_t2.dat', delimiter=',', unpack=True)
+XP3H = (0,1,2,3,4,5,6,7,8)
+YP3H = np.loadtxt('handcheck_p_dist_t3.dat', unpack=True)
+XP3, YP3 = np.loadtxt('p_dist_t3.dat', delimiter=',', unpack=True)
 
-X2, Y2 = np.loadtxt('p_dist_t3.dat', delimiter=',', unpack=True)
-X2 = (0,1,2,3,4,5,6,7,8,9)
+#self-intermediate scattering function, data vs hand check
+XF1H = (0,1,2,3,4)
+YF1H = np.loadtxt('handcheck_f_s_q1.dat', unpack=True)
+XF1, YF1 = np.loadtxt('f_s_q1.dat', delimiter=',', unpack=True)
+XF2H = (0,1,2,3,4)
+YF2H = np.loadtxt('handcheck_f_s_q2.dat', unpack=True)
+XF2, YF2 = np.loadtxt('f_s_q2.dat', delimiter=',', unpack=True)
+XF3H = (0,1,2,3,4)
+YF3H = np.loadtxt('handcheck_f_s_q3.dat', unpack=True)
+XF3, YF3 = np.loadtxt('f_s_q3.dat', delimiter=',', unpack=True)
 
 
-plt.scatter(X1, Y1, color='cyan')
-#plt.Line2D(X1, Y1, color='red')
-plt.scatter(X2, Y2, color='orange')
+plt.scatter(XMSH, YMSH, color='cyan')
+plt.scatter(XMS, YMS, color='orange')
 
-plt.title('Probability Distribution at t=3s')
-plt.xlabel('displacement (arb. units)')
-plt.ylabel('Probability')
-plt.legend(["Data","Hand Check"])
+plt.title('Mean Square Displacement')
+plt.xlabel('time')
+plt.ylabel('displacement')
+plt.legend(["Hand Check","Data"])
 plt.show()
