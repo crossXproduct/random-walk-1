6d3a44a85095a1e31c194623d3be0a87fd8118a9
Elise Aaron <ea554820@ohio.edu>
2021-07-07 11:48:32 -0400

    Generator creates new directory for history files. Untested.

--- randomwalk.cpp
+++ randomwalk.cpp
@@ -16,70 +16,77 @@
 #include <cmath>
 #include <vector>
 #include <ctime>
-
+#include <sys/stat.h>
+#include <sys/types.h>
 using namespace std;
 
 ///FUNCTION DECLARATIONS (DEFINITIONS BELOW MAIN)
-void runSpecs(int &num_histories, int &num_steps); //Process user input for number of runs and steps per run
-void history(int num_steps, string filename); //Generate a single history and print to file
-void recordHistories(int num_histories, int num_steps); //Generate histories and generate datafiles for specified number of runs
-string getFilename(); //Generate destination filename for data
+void runSpecs(int &num_histories, int &num_steps, char * folder); //Process user input for number of runs and steps per run
+void history(const int &num_steps, const string &filename); //Generate a single history and print to file
+void recordHistories(const int &num_histories, const int &num_steps, char * &folder); //Generate histories and generate datafiles for specified number of runs
+string createPath(char * &folder); //Generate destination filename for data
 
 
 int main() {
     int num_histories;
     int num_steps;
-    runSpecs(num_histories, num_steps);
-    recordHistories(num_histories, num_steps);
+    char *folder;
+    runSpecs(num_histories, num_steps, folder);
+    recordHistories(num_histories, num_steps, folder);
     return 0;
 } ///main
 
-void runSpecs(int &num_histories, int &num_steps) {
+void runSpecs(int &num_histories, int &num_steps, char * folder) {
     cout << "Number of histories: ";
     cin >> num_histories;
     cout << "Number of steps per history: ";
     cin >> num_steps;
+    cout << "Folder: ";
+    cin >> folder;
 }
 
-void history(int num_steps, string filename) {
+void history(const int &num_steps, const string &filename) {
     ///Open file stream
     ofstream file;
     file.open(filename);
 
     ///Generate
-    int step = 0;
+    int r = 0;
     int r_total = 0;
     file << r_total;
     for(int i = 1; i <= num_steps; i++){
         file << endl;
-        step = (rand() % 2) * 2 - 1;
-        r_total += step;
+        r = (rand() % 2) * 2 - 1;
+        r_total += r;
         file << r_total;
     }
     ///Close file stream
     file.close();
 }
 
-void recordHistories(int num_histories, int num_steps) {
+void recordHistories(const int &num_histories, const int &num_steps, char * &folder) {
     for(int i = 0; i < num_histories; i++) {
         srand(i);
-        history(num_steps, getFilename());
+        history(num_steps, createPath(folder));
     }
 }
 
-string getFilename() {
-    ifstream name;
+string createPath(char * &folder) {
+    ifstream file;
     char number1 = ' ' - 32; //ASCII number 0
     char number2 = ' ' - 32;
-    string fullname = "";
+    string fullpath = "";
+
+    if (mkdir(folder, 0777) == -1)
+        cerr << "Error :  " << strerror(errno) << endl;
+
+    else cout << "Folder " << folder << " created." << endl;
 
     do { //Search for filename that does not yet exist, and create the file
 
-
-
-        if(!name.eof()) {
-            name.close();
-            name.clear();
+        if(!file.eof()) {
+            file.close();
+            file.clear();
         }
 
         if(number2 == 9) {
@@ -88,11 +95,11 @@ string getFilename() {
             }
         else number2++;
 
-        fullname = "history" + to_string(number1) + to_string(number2) + ".dat";
-        name.open(fullname);
+        fullpath = to_string(folder) + "/history" + to_string(number1) + to_string(number2) + ".dat";
+        file.open(fullpath);
 
-    } while(name.peek() != EOF); //if file is NOT blank, try again
+    } while(file.peek() != EOF); //if file is NOT blank, try again
 
-    name.close();
-    return fullname;
+    file.close();
+    return fullpath;
 }
